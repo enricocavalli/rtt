4955858dde6feee7830f0e31d4188f734c147087
Enrico Cavalli <cavalli@cilea.it>
2012-05-12 14:06:08 +0200

    modificati link per nuova struttura
    aggiornata anche la documentazione

--- www/doMeasure.php
+++ www/doMeasure.php
@@ -6,7 +6,7 @@ $config = RTT_Configuration::getInstance();
 foreach( $m->getSiteNames() as $site ) {
     
 	
-	$url = RTT_Utilities::getBaseURL() . 'index.php/measure/singleMeasure.php?site=' . $site;
+	$url = RTT_Utilities::getBaseURL() . 'singleMeasure.php?site=' . $site;
 	echo '<iframe src="' . $url . '" style="display:none"></iframe>';
 	
 }
